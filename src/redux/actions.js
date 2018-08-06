a0236a3ed5a2e432b83bc19f243a85bb09650c09
PahanZ <zagvzdin.pakhan.webdev@gmail.com>
2018-08-07 00:03:40 +0300

    typing actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,11 +1,27 @@
 // @flow
 
-const createBlock = (block: Object): Object => ({
+type ActionTypes<T> =
+  | { type: "CREATE_BLOCK", payload: T }
+  | { type: "CREATE_LINE", payload: T }
+
+
+  type blockType = {
+    coordX: string,
+    coordY: string
+  }
+
+  type lineType = {
+    firstPoint: string,
+    secondPoint: string
+  }
+
+
+const createBlock = (block: blockType): ActionTypes<blockType> => ({
   type: 'CREATE_BLOCK',
   payload: block,
 });
 
-const createLine = (line: Object): Object => ({
+const createLine = (line: lineType): ActionTypes<lineType> => ({
   type: 'CREATE_LINE',
   payload: line,
 });
